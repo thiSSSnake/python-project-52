ccb816fd3fcfb5fbe476175d9e07a21ec8d406f1
Andrei Emelianenko <spliitvein@gmail.com>
2024-04-27 18:28:05 +0300

    collectstatic

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -o errexit
 make install
 
 # Convert static asset files
-python manage.py collectstatic --no-input
+python3 manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python manage.py migrate
+python3 manage.py migrate

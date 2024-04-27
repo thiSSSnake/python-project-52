8d6b26a0c4a605b0ea0c01171866b84f3fdc9b19
Andrei Emelianenko <spliitvein@gmail.com>
2024-04-27 22:41:49 +0300

    trying to fix staticfiles

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = BASE_DIR / 'app/public/static'
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type

95986491e0896f4bb746bc99ee803c7bb17d1e74
Andrei Emelianenko <spliitvein@gmail.com>
2024-04-27 22:02:24 +0300

    add requirements.txt

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 make install
+pip install -r requirements.txt
 
 # Convert static asset files
 python3 manage.py collectstatic --no-input

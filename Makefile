f4ae48e9ae2ff7b115e43bcbdb1408536db04d7f
Andrei Emelianenko <spliitvein@gmail.com>
2024-04-28 04:17:52 +0300

    modified:   Makefile         modified:   poetry.lock         modified:   pyproject.toml         new file:   task_manager/locale/ru/LC_MESSAGES/django.mo         new file:   task_manager/locale/ru/LC_MESSAGES/django.po         modified:   task_manager/settings.py         modified:   task_manager/templates/index.html

--- Makefile
+++ Makefile
@@ -1,2 +1,11 @@
 install:
 	poetry install
+
+start:
+	python3 manage.py runserver
+
+local:
+	poetry run django-admin makemessages --ignore="static" --ignore=".env"  -l ru
+
+translate:
+	poetry run django-admin compilemessages
--- task_manager/settings.py
+++ task_manager/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_bootstrap5',
     'task_manager',
 ]
 
@@ -116,7 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'UTC'
 

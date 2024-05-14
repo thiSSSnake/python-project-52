6ad3be0e3e718c9ce9f1bbf3b7789ff0b4580616
Andrei Emelianenko <spliitvein@gmail.com>
2024-05-14 19:48:08 +0300

    why you check for sqlite3 ?

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -93,21 +93,20 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
-        'default': dj_database_url.config(
-            # Replace this value with your local database's connection string.
-            default=DATABASE_URL,
-            conn_max_age=600
-        )
-    }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+DATABASES = {
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default=DATABASE_URL,
+        conn_max_age=600
+    )
+}
 
 
 # Password validation
